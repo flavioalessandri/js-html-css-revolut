68318eb82b79c8c7cea438438917b104ca7777fe
Flavio <flavio.alessandri@gmail.com>
2020-07-07 14:04:55 +0200

    Revolut - css html responsive nav - work in progress

--- js/main.js
+++ js/main.js
@@ -1,38 +1,22 @@
 $(document).ready(function(){ //open jquery library when DOM is fully loaded
 
-var parent_dropdown =  $('.link .parent_dropdown');
+var parent_dropdown =  $('#navbar-content .link .parent_dropdown');
 console.log(parent_dropdown);
 
-var dropdown= $('.dropdown' );
+var dropdown= $('#navbar-content ul.dropdown' );
 console.log(dropdown);
 
-var dropdown_items = $('.dropdown > li').children();
+var dropdown_items = $('#navbar-content .dropdown > li').children();
 console.log(dropdown_items);
-
-parent_dropdown.on('mouseover', function() {//when mouse over the class parent_dropdown
-  parent_dropdown.removeClass('active'); //remove all .active class from other element with parent_dropdown
-  parent_dropdown.children('.dropdown').addClass('hidden'); //make sure all dropdown are hidden
-  $(this).addClass('active'); //add active class to this element
+//
+parent_dropdown.on('mouseenter', function() {//when mouse over the class parent_dropdown
+  $(this).children('.dropdown').addClass('active');
   $(this).children('.dropdown').removeClass('hidden'); //make the dropdown of this element visible
+})
+
+parent_dropdown.on('mouseleave', function() {//when mouse over the class parent_dropdown
+  $(this).children('.dropdown').removeClass('active');
+  $(this).children('.dropdown').addClass('hidden'); //make the dropdown of this element visible
+})
+
 });
-
-dropdown.on('mouseout', function() {
-  parent_dropdown.removeClass('active');
-  $(this).addClass('hidden');
-  alert("no");
-});
-
-// dropdown_items.on('mouseover', function() {//when mouse over the element of dropdown
-//   dropdown_items.removeClass('active');
-//   $(this).addClass('active');
-// });
-
-
-
-
-
-
-
-
-
-  });
