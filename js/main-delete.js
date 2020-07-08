62236d37d951f4cf011f2413047c705f17bd7f73
Flavio <flavio.alessandri@gmail.com>
2020-07-08 18:31:00 +0200

    Revolut - responsive part

--- js/main-delete.js
+++ js/main-delete.js
@@ -1,56 +1,59 @@
 $(document).ready(function(){ //open jquery library when DOM is fully loaded
 
-part to make the dropdown menu visible after mouseenter the link;
-var parent_dropdown =  $('#navbar-content .link .parent_dropdown');
-console.log(parent_dropdown);
+// Mediaquery max-width 1025px
+// use the hamburger menu to show the dropdown items
 
-var dropdown= $('#navbar-content ul.dropdown' );
-console.log(dropdown);
+var hamburger_menu = $('#hamburger-menu');
+hamburger_menu.click(function() {//when mouse over the class parent_dropdown
 
-var dropdown_items = $('#navbar-content .dropdown > li').children();
-console.log(dropdown_items);
-
-var width = $('header').width();
-
-parent_dropdown.on('mouseenter', function() {//when mouse over the class parent_dropdown
-  if (width>1025) {
-    $(this).children('.dropdown').addClass('active');
-    $(this).children('.dropdown').removeClass('hidden'); //make the dropdown of this element visible
-  }
-});
-
-
-
-parent_dropdown.on('mouseleave', function() {//when mouse over the class parent_dropdown
-  if ($(window).width()>1025) {
-    $(this).children('.dropdown').removeClass('active');
-    $(this).children('.dropdown').addClass('hidden'); //make the dropdown of this element visible
-  }else{}
-});
-
-$('#hamburger-menu').click(function() {//when mouse over the class parent_dropdown
+    $(this).toggleClass('hamb-position-fixed');
     $('#navbar-info-wrapper').toggle();
+    // $('#navbar-link-wrapper').toggle();
     $('#navbar-link .link').toggle();
     $("#hamb-line01").toggleClass("anim1");
     $("#hamb-line02").toggleClass("anim2");
 });
 
-$('#navbar-link .parent_dropdown > i').click(function() {//when mouse over the class parent_dropdown
-  if ($(window).width()<1025) {
-    $(this).toggleClass('fa-chevron-up');
-    $(this).next().toggle(400);
-     //make the dropdown of this element visible
-  }
-})
+// Mediaquery max-width 1025px
+// use the arrow to show the dropdown menu on click
 
-// part to lead the behaviour of the navbar-wrapper
-$(window).scroll(function(){
-  if($(this).scrollTop() > 20){
-    $('#navbar-wrapper').addClass('fixed');
-  }else if($(this).scrollTop() < 20){
-    $('#navbar-wrapper').removeClass('fixed');
-  }
-});
+  $('#navbar-link .parent_dropdown > i').click(function() {//when mouse over the class parent_dropdown
 
+      $(this).toggleClass('fa-chevron-up'); //arrow change the position to up after click
+      $(this).next().toggle(400);
+
+  });
+
+  // Mediaquery min-width 1026px
+
+  // var for container of dropdown list
+  var parent_dropdown =  $('#navbar-content .link .parent_dropdown');
+  console.log(parent_dropdown);
+
+  // var for  dropdown list
+  var dropdown= $('#navbar-content ul.dropdown' );
+  console.log(dropdown);
+
+  // var for  windows eidth - doesn't work
+  var width = $(window).width();
+
+
+  // function when mouse enter on the parent-dropdown
+  parent_dropdown.on('mouseenter', function() {//when mouse over the class parent_dropdown
+
+      $(this).children('span').addClass('color');
+      $(this).children('.dropdown').addClass('active');
+      $(this).children('.dropdown').removeClass('hidden'); //make the dropdown of this element visible
+
+  });
+
+// function when mouse leave the parent-dropdown
+  parent_dropdown.on('mouseleave', function() {//when mouse over the class parent_dropdown
+
+      $(this).children('span').removeClass('color');
+      $(this).children('.dropdown').removeClass('active');
+      $(this).children('.dropdown').addClass('hidden'); //make the dropdown of this element visible
+
+  });
 
 });
